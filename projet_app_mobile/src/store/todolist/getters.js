c3d5299bbcc3f57ca3bc41e9f157c869e734c62c
MarionGrassi <mariongrassi11@gmail.com>
2021-03-24 21:34:25 +0100

    Correction d'un petit bug lors de la creation d'une liste

--- projet_app_mobile/src/store/todolist/getters.js
+++ projet_app_mobile/src/store/todolist/getters.js
@@ -32,15 +32,17 @@ export const getUncompletedOnly = (state) => {
     return state.todos.filter(todo => !todo.completed);
 }
 
-export const getUncompletedTodo = (state) => (id) =>{
-    return state.todoLists.find(todoList => todoList.id === id).todos.filter( toudou => !toudou.completed).length;
+export const getUncompletedTodo = (state) => (id) => {
+    let lists = state.todoLists.find(todoList => todoList.id === id).todos;
+    return lists != null ? lists.filter( toudou => !toudou.completed).length : 0;
 }
 
 export const getAllUncompleted = (state) =>{
     var acc = 0;
     for(const list in state.todoLists){
-        acc += (state.todoLists[list].todos.filter(toudou => !toudou.completed).length);
-        
+        if(state.todoLists[list].todos != null) {
+            acc += (state.todoLists[list].todos.filter(toudou => !toudou.completed).length);
+        }
     }
     return acc;
 }
